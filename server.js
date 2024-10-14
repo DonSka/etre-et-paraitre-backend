25cffdb354c20ebcca3fa40fbcc08677f0dd3f51
Donatello SCARABINO <donatelloscarabino@outlook.fr>
2024-10-14 23:56:40 +0200

    fin d'un round

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ let questions = [
   {
     id: 1,
     round_id: 1,
-    name: "Vos vrais amis, vous les comptez ...",
+    name: "vos vrais amis, vous les comptez ...",
     answer_1: "Sur les doigts d'une main",
     answer_2: "Sur les deux mains",
     answer_3: "Vous n'avez pas assez de doigts pour les compter",
@@ -50,7 +50,7 @@ let questions = [
   {
     id: 2,
     round_id: 1,
-    name: "À quelle fréquence vous observez-vous à travers un miroir ou des photos ?",
+    name: "à quelle fréquence vous observez-vous à travers un miroir ou des photos ?",
     answer_1: "Plus souvent que la plupart des gens",
     answer_2: "Moins souvent que la plupart des gens",
     answer_3: null,
@@ -59,7 +59,7 @@ let questions = [
   {
     id: 3,
     round_id: 1,
-    name: "Mentez-vous ?",
+    name: "mentez-vous ?",
     answer_1: "Plus que la plupart des gens",
     answer_2: "Moins que la plupart des gens",
     answer_3: null,
@@ -67,8 +67,11 @@ let questions = [
   },
 ];
 
-function getRandomQuestion(roundId) {
-  const roundQuestions = questions.filter((q) => q.round_id === roundId);
+function getRandomQuestion(game) {
+  const roundQuestions = questions.filter(
+    (q) =>
+      q.round_id === game.currentRound.id && !game.posedQuestions.includes(q.id)
+  );
   return roundQuestions[Math.floor(Math.random() * roundQuestions.length)];
 }
 
@@ -122,6 +125,17 @@ app.post("/create-game", (req, res) => {
   let game = games.find((g) => g.pin === pin);
 
   if (game) {
+    const existingPlayer = game.players.find(
+      (player) => player.username === username
+    );
+
+    if (existingPlayer) {
+      return res.status(400).json({
+        message:
+          "Ce pseudo est déjà utilisé dans cette partie. Veuillez choisir un autre pseudo.",
+      });
+    }
+
     const newPlayer = {
       id: game.players.length + 1,
       username: username,
@@ -148,6 +162,8 @@ app.post("/create-game", (req, res) => {
       id: games.length + 1,
       pin: pin,
       players: [newPlayer],
+      currentRound: rounds.find((round) => round.id === 1),
+      posedQuestions: [],
     };
 
     games.push(newGame);
@@ -168,21 +184,27 @@ app.post("/start-game", (req, res) => {
   let game = games.find((g) => g.pin === pin);
 
   if (game) {
-    const currentRound = rounds[0];
-    const currentQuestion = getRandomQuestion(currentRound.id);
-    const roundPlayer = game.players[0];
+    const currentQuestion = getRandomQuestion(game);
+    const roundPlayer = game.players.find((player) => player.id === 1);
+
+    game.players = game.players.map((player) => ({
+      ...player,
+      isTurn: player.id === roundPlayer.id,
+    }));
 
     io.to(pin).emit("game-started", {
-      currentRound,
       currentQuestion,
       roundPlayer,
+      currentRound: game.currentRound,
+      gamePlayers: game.players,
     });
 
     return res.json({
       message: "Le jeu a démarré",
-      currentRound,
       currentQuestion,
       roundPlayer: roundPlayer,
+      currentRound: game.currentRound,
+      gamePlayers: game.players,
     });
   } else {
     return res.status(404).json({ message: "Partie non trouvée" });
@@ -202,7 +224,10 @@ app.post("/submit-answer", (req, res) => {
 
     io.to(pin).emit("right-answer-submitted", rightAnswer);
 
-    return res.json({ message: "Bonne réponse soumise", rightAnswer });
+    return res.json({
+      message: "Bonne réponse soumise",
+      right_answer: rightAnswer,
+    });
   } else {
     return res.status(404).json({ message: "Partie non trouvée" });
   }
@@ -240,6 +265,58 @@ app.post("/submit-guess", (req, res) => {
       hasAnswered: true,
       answer: guessedAnswer,
     });
+  } else {
+    return res.status(404).json({ message: "Partie non trouvée" });
+  }
+});
+
+app.post("/next-turn", (req, res) => {
+  const { pin } = req.body;
+
+  let game = games.find((g) => g.pin === pin);
+
+  if (game) {
+    const allPlayersPlayed = game.players.every((player) => player.isTurn);
+
+    if (allPlayersPlayed) {
+      io.to(pin).emit("round-ended", {
+        message: "Tous les joueurs ont joué, la manche est terminée.",
+        gamePlayers: game.players,
+      });
+
+      return res.json({
+        message: "La manche est terminée.",
+        gamePlayers: game.players,
+      });
+    } else {
+      const nextPlayer = game.players.filter((player) => !player.isTurn)[0];
+
+      game.players = game.players.map((player) => ({
+        ...player,
+        has_answered: false,
+        answer: "",
+      }));
+
+      game.players = game.players.map((p) =>
+        p.id === nextPlayer.id ? { ...p, isTurn: true } : p
+      );
+
+      const currentQuestion = getRandomQuestion(game);
+      game.posedQuestions.push(currentQuestion.id);
+
+      io.to(pin).emit("next-turn", {
+        currentQuestion,
+        roundPlayer: game.players.find((p) => p.id === nextPlayer.id),
+        gamePlayers: game.players,
+      });
+
+      return res.json({
+        message: "Tour suivant",
+        currentQuestion,
+        roundPlayer: game.players.find((p) => p.id === nextPlayer.id),
+        gamePlayers: game.players,
+      });
+    }
   } else {
     return res.status(404).json({ message: "Partie non trouvée" });
   }
